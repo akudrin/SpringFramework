1c498be201bf7c817d60cc2ea3b1555441a19c41
Andrei Kudrin <andreikudrin@gmail.com>
2019-01-28 14:50:46 +0000

    Spring Beans with Java Code

--- src/com/akudrin/springdemo/annotations/SportConfig.java
+++ src/com/akudrin/springdemo/annotations/SportConfig.java
@@ -1,10 +1,24 @@
 package com.akudrin.springdemo.annotations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.akudrin.springdemo.annotations")
 public class SportConfig {
+	// define bean for our sad fortune service
+		@Bean
+		public FortuneService sadFortuneService() {
+			return new SadFortuneService();
+		}
+		
+		// define bean for our swim coach AND inject dependency
+		@Bean
+		public Coach swimCoach() {
+			SwimCoach mySwimCoach = new SwimCoach(sadFortuneService());
+			
+			return mySwimCoach;
+		}
 	
 }
