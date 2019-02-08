c9efcda766a8db62f12592c72894c22d3ea47661
Andrei Kudrin <andreikudrin@gmail.com>
2019-02-08 11:21:36 +0000

    hibernate - @OneToMany Bi-Directional

--- SpringFrameworkHibernate/src/com/akudrin/hibernate/demo/entity/Instructor.java
+++ SpringFrameworkHibernate/src/com/akudrin/hibernate/demo/entity/Instructor.java
@@ -1,12 +1,17 @@
 package com.akudrin.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -45,6 +50,13 @@ public class Instructor {
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
+	
+	@OneToMany(fetch=FetchType.LAZY,
+			   mappedBy="instructor",
+			   cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+						 CascadeType.DETACH, CascadeType.REFRESH})
+	private List<Course> courses;
+	
 	
 	public Instructor() {
 		
@@ -100,6 +112,26 @@ public class Instructor {
 	public String toString() {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+	
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+	// add convenience methods for bi-directional relationship
+	 public void add(Course tempCourse) {
+		
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
 	}
 	
 	
