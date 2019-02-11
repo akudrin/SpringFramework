deab2ff5596c64b782af852d84414e0d0fe8acc7
Andrei Kudrin <andreikudrin@gmail.com>
2019-02-11 12:43:20 +0000

    hibernate - @OneToMany UniDirectional

--- SpringFrameworkHibernate/src/com/akudrin/hibernate/demo/entity/Course.java
+++ SpringFrameworkHibernate/src/com/akudrin/hibernate/demo/entity/Course.java
@@ -42,7 +42,9 @@ public class Course {
 	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 	
-	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id")
+	private List<Review> reviews;
 		
 	public Course() {
 		
@@ -76,6 +78,24 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	// add a convenience method
+	
+	public void addReview(Review theReview) {
+	
+		if (reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		
+		reviews.add(theReview);
+	}
 
 	@Override
 	public String toString() {
