22594b46a04cd2445e46371ee51980a9cb13c711
Andrei Kudrin <andreikudrin@gmail.com>
2019-02-25 10:25:49 +0000

    spring mvc + hibernate - added delete customer button

--- spring-mvc-crud/src/com/akudrin/springdemo/controller/CustomerController.java
+++ spring-mvc-crud/src/com/akudrin/springdemo/controller/CustomerController.java
@@ -69,6 +69,15 @@ public class CustomerController {
 		return "customer-form";
 	}
 	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int theId) {
+		
+		// delete the customer
+		customerService.deleteCustomer(theId);
+		
+		return "redirect:/customer/list";
+	}
+	
 }
 
 
