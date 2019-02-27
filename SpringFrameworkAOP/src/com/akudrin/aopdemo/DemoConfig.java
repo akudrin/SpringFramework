d09bd3a05659aeb8296fb80208ed1801f12ac5a4
Andrei Kudrin <andreikudrin@gmail.com>
2019-02-27 10:22:14 +0000

    spring AOP - @Before Advice Type

---
+++ SpringFrameworkAOP/src/com/akudrin/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.akudrin.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.akudrin.aopdemo")
+public class DemoConfig {
+
+}
