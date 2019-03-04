27056473732223ef7c0f2c7c3090bd6461805966
Andrei Kudrin <andreikudrin@gmail.com>
2019-03-04 07:34:10 +0000

    AOP - Pointcut Expressions - Match on One or More Param

--- SpringFrameworkAOP/src/com/akudrin/aopdemo/dao/AccountDAO.java
+++ SpringFrameworkAOP/src/com/akudrin/aopdemo/dao/AccountDAO.java
@@ -2,10 +2,12 @@ package com.akudrin.aopdemo.dao;
 
 import org.springframework.stereotype.Component;
 
+import com.akudrin.aopdemo.Account;
+
 @Component
 public class AccountDAO {
 
-	public void addAccount() {
+	public void addAccount(Account theAccount, boolean vip) {
 		
 		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
 		
