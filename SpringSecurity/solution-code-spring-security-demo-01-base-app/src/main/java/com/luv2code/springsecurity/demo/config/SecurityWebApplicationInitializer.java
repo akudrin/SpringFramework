7d220bf826b2f91e367f79f81a8c11ac24c77934
Andrei Kudrin <andreikudrin@gmail.com>
2019-04-27 20:49:25 +0100

    added Bootstrap 3 styling to my login form

--- SpringSecurity/solution-code-spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
+++ SpringSecurity/solution-code-spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -2,6 +2,8 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+//Spring Security Initializer Class to register the Spring Security Filters
+
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
