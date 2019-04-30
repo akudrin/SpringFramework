259f1df4067e95579cb0da45fbcf70553a6d5d04
Andrei Kudrin <andreikudrin@gmail.com>
2019-04-30 09:49:23 +0100

    created custom access denied page

--- SpringSecurity/solution-code-spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/controller/DemoController.java
+++ SpringSecurity/solution-code-spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/controller/DemoController.java
@@ -6,23 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoController {
 
-	        @GetMapping("/")
-        public String showLanding() {
-            return "landing";
-        }
-        
-         @GetMapping("/systems")
-        public String showSystems() {
-            return "systems";
-        }
-        
-             @GetMapping("/leaders")
-        public String showLeaders() {
-            return "leaders";
-        }
-     
-        @GetMapping("/employees")
-        public String showHome() {
-            return "home";
-        }
+    @GetMapping("/")
+    public String showLanding() {
+        return "landing";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
+    @GetMapping("/employees")
+    public String showHome() {
+        return "home";
+    }
 }
