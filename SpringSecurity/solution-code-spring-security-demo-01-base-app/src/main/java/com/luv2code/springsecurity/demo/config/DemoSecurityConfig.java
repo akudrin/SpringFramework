74583ce6fbbd0ebb4bebac10938fec865b9ee4da
Andrei Kudrin <andreikudrin@gmail.com>
2019-05-01 10:43:41 +0100

    added JDBC authentication

--- SpringSecurity/solution-code-spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ SpringSecurity/solution-code-spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,18 +1,37 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 //Class to create Spring Security Configuration
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // add a reference to our security data source
+	
+	@Autowired
+	private DataSource securityDataSource;
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// use jdbc authentication ... oh yeah!!!
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
+	}
+
+    
+    
+    
+   /* 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
@@ -24,6 +43,7 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
                 .withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
     }
+    */
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
