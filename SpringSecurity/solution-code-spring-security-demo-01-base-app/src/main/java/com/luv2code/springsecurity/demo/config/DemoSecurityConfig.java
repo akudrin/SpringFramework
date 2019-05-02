97c67a59b316c494d6d098b0c41085fb316c88ff
Andrei Kudrin <andreikudrin@gmail.com>
2019-05-02 09:58:23 +0100

    Added User Registration Form with Custom Database Tables

--- SpringSecurity/solution-code-spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ SpringSecurity/solution-code-spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,18 +1,27 @@
 package com.luv2code.springsecurity.demo.config;
 
+import com.luv2code.springsecurity.demo.service.UserService;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //Class to create Spring Security Configuration
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // add a reference to our security data source
+    @Autowired
+    private UserService userService;
+    
+    
     // add a reference to our security data source
 	
 	@Autowired
@@ -28,7 +37,7 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 	}
 
-    
+        
     
     
    /* 
@@ -62,5 +71,22 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling().accessDeniedPage("/access-denied");;
     }
+    
+    //beans
+	//bcrypt bean definition
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	//authenticationProvider bean definition
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(userService); //set the custom user details service
+		auth.setPasswordEncoder(passwordEncoder()); //set the password encoder - bcrypt
+		return auth;
+	}
+	  
 
 }
